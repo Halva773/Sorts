c2f6539bb9fe92df1c34e2c012377ac91efe56cc
Artem-HN <48379705+Halva773@users.noreply.github.com>
2020-06-04 14:14:57 +0300

    extra update

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BubbleSort
+{
+    class BubbleSort
+    {
+        public void Sort(int[] a)
+        {
+            for(int i = a.Length - 1; i >= 0; i--)
+                for(int y = 0;y < i; y++)
+                {
+                    if (a[y] > a[y + 1]) 
+                    {
+                        int trash = a[y];
+                        a[y] = a[y+1];
+                        a[y + 1] = trash;
+                    }
+                }
+            for (int i = 0; i < a.Length; i++) Console.WriteLine(a[i]); // Entering mas
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Bubble = new BubbleSort();
+
+            int[] mas = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray(); // Reading mas
+
+            Bubble.Sort(mas);
+
+            
+        }
+    }
+}
